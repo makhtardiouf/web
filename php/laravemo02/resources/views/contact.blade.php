edea662d11c8ebafe374eba7332cc3416e2a33af
Makhtar Diouf <makhtar.diouf@gmail.com>
2017-06-12 11:39:12 +0900

    Stepping up Laravel 5.4

---
+++ php/laravemo02/resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', 'Contact us')
+
+@section('content')
+<div class="content">
+  
+    <?php
+   // var_dump($data);
+     foreach ($data as $key => $value) {
+         echo "Client IP [$key]: " . $value . PHP_EOL;
+   
+    }
+    ?>
+</div>
+
+@endsection
