78b81b0574f4fd2321615dc7f0ceb5200551da01
IbnMakhtoum <makhtar.diouf@gmail.com>
2020-10-17 23:26:28 +0000

    Added the Square, Board and Game components. Layout done, implemented up to winner evaluation

---
+++ react-game/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Game from './Game';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          First steps in the React world...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <div>
+        <Game />
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
